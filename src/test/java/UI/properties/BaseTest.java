a45da0c65903f84cd133e7e909f55bc0b73eac25
Oleg Bazhenov <flying_panda@list.ru>
2025-11-23 23:03:32 +0200

    Обновление SetUp в BaseTest для корректной работы с Jenkins

--- src/test/java/UI/properties/BaseTest.java
+++ src/test/java/UI/properties/BaseTest.java
@@ -7,17 +7,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 abstract public class BaseTest {
 
     public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-plugins");
+        options.addArguments("--mute-audio");
+
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.webdriverLogsEnabled = true;
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.headless = true;
+        Configuration.headless = false;
     }
 
     @BeforeEach
