657688779e68efb4406fe17a696561565130e150
Oleg Bazhenov <flying_panda@list.ru>
2025-11-23 23:37:25 +0200

    Обновление SetUp в BaseTest для корректной работы с Jenkins 3

--- src/test/java/UI/properties/BaseTest.java
+++ src/test/java/UI/properties/BaseTest.java
@@ -9,23 +9,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.File;
-
 abstract public class BaseTest {
 
     public void setUp(){
-        String tmpDir = "/var/jenkins_home/tmp";
-        new File(tmpDir).mkdirs();
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless=new");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-plugins");
         options.addArguments("--mute-audio");
-        options.addArguments("--user-data-dir=" + tmpDir);
-        options.addArguments("--remote-debugging-port=9222");
 
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
